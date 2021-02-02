5f58bb5bec597fb8443dfa39919de48f1c6ea33c
Eli T. Drumm <eli@etd.is>
2021-02-01 21:22:25 -0500

    on the way to sequelize v6

--- Db.js
+++ Db.js
@@ -8,33 +8,33 @@ const Sequelize= require('sequelize');
 const database = new Sequelize (process.env.DATABASE_URL, {
     dialect:'postgres'});
 
-const ServiceEntry = database.import('./Models/service')
-const Teacher = database.import('./Models/teacherUser')
-const Student = database.import('./Models/studentUser');
-const Events = database.import('./Models/events');
+// const ServiceEntry = database.import('./Models/service')
+// const Teacher = database.import('./Models/teacherUser')
+// const Student = database.import('./Models/studentUser');
+// const Events = database.import('./Models/events');
 
-//One to Many
-Teacher.hasMany(Student, {foreignKey: "classId"})
-Student.belongsTo(Teacher, {foreignKey: "classId"})
+// //One to Many
+// Teacher.hasMany(Student, {foreignKey: "classId"})
+// Student.belongsTo(Teacher, {foreignKey: "classId"})
 
-//One to Many
-Teacher.hasMany(Events, {foreignKey: "classId"})
-Events.belongsTo(Teacher, {foreignKey: "classId"})
+// //One to Many
+// Teacher.hasMany(Events, {foreignKey: "classId"})
+// Events.belongsTo(Teacher, {foreignKey: "classId"})
 
-//One to Many
-Teacher.hasMany(ServiceEntry, {foreignKey: "classId"})
-ServiceEntry.belongsTo(Teacher, {foreignKey: "classId"})
-
-
-//One to Many
-Student.hasMany(ServiceEntry)
-ServiceEntry.belongsTo(Student)
+// //One to Many
+// Teacher.hasMany(ServiceEntry, {foreignKey: "classId"})
+// ServiceEntry.belongsTo(Teacher, {foreignKey: "classId"})
 
 
+// //One to Many
+// Student.hasMany(ServiceEntry)
+// ServiceEntry.belongsTo(Student)
 
 
-database.authenticate()
-    .then(()=> console.log('postgres db is connected'))
-    .catch(err=> console.log(err));
 
-    module.exports=database;
+
+// database.authenticate()
+//     .then(()=> console.log('postgres db is connected'))
+//     .catch(err=> console.log(err));
+
+module.exports=database;
--- Models/events.js
+++ Models/events.js
@@ -1,33 +1,60 @@
-module.exports=(sequelize, DataTypes) =>{
-    const Events= sequelize.define('events', {
-        date:{
-            type:DataTypes.STRING,
-            // allowNull: false,
-            // validate:{notEmpty:true}
-        },
+
+const { DataTypes } = require('sequelize')
+
+
+module.exports = require('../Db').define('event', {
+    date: {
+        type: DataTypes.STRING
+    },
+    description: {
+        type: DataTypes.STRING
+    },
+    location: {
+        type: DataTypes.STRING
+    },
+    title: {
+        type: DataTypes.STRING
+    },
+    hours: {
+        type: DataTypes.INTEGER,
+        validate: 
+    }
+})
+
+
+
+
+
+// module.exports = (sequelize, DataTypes) =>{
+//     const Events= sequelize.define('events', {
+//         date:{
+//             type:DataTypes.STRING,
+//             // allowNull: false,
+//             // validate:{notEmpty:true}
+//         },
     
-        description:{
-            type:DataTypes.STRING,
-            // allowNull:false
-        },
-       location:{
-            type:DataTypes.STRING,
-            // allowNull:true
-        },    
-        title:{
-            type:DataTypes.STRING,
-            // allowNull:false
-        },  
+//         description:{
+//             type:DataTypes.STRING,
+//             // allowNull:false
+//         },
+//        location:{
+//             type:DataTypes.STRING,
+//             // allowNull:true
+//         },    
+//         title:{
+//             type:DataTypes.STRING,
+//             // allowNull:false
+//         },  
       
 
-        hours:{
-            type:DataTypes.INTEGER,
-            // allowNull:true,
-            // validate:{notEmpty:true},
+//         hours:{
+//             type:DataTypes.INTEGER,
+//             // allowNull:true,
+//             // validate:{notEmpty:true},
     
-        }
+//         }
 
         
-    })
-    return Events
-}
+//     })
+//     return Events
+// }
